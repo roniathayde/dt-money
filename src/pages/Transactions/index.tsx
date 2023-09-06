980affb71267c17e29c56dbfa39f6a631627ce77
Roni Athayde <pontes014@gmail.com>
2023-09-05 21:52:03 -0300

    feat:add a searchform container

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./SearchForm";
+import { PriceHightlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income" >
+                  R$ 12.000,00
+                </PriceHightlight>
+              </td>
+              <td >Venda</td>
+              <td >13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHightlight variant="outcome" >
+                  R$ 12.000,00
+                </PriceHightlight>
+              </td>
+              <td >Alimentação</td>
+              <td >10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
