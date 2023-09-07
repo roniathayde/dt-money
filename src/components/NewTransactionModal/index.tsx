deb509997aff5f775d84acb8e01030b5bb0fbc2e
Roni Athayde <pontes014@gmail.com>
2023-09-07 00:48:51 -0300

    styles: adda modal buttons income and outcome radiobuttons with radix

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export function NewTransactionModal (){
   return(
@@ -19,6 +20,18 @@ export function NewTransactionModal (){
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
           
+          <TransactionType>
+            <TransactionTypeButton variant='income'value="income">
+              <ArrowCircleUp size={24}/>
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome' value="outcome">
+              <ArrowCircleDown size={24}/>
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">
             Cadastrar
           </button>
